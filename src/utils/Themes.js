01c45a00f6f4cdded6bc1b7bb04ab2982fd311b1
Chhavigupta2401 <chhavigupta2401@gmail.com>
2024-04-17 18:26:58 +0530

    finish

---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+    bg:"#1C1C27",
+    bgLight: "#1C1E27",
+    primary:"#854CE6",
+    text_primary:"#F2F3F4",
+    text_secondary:"#b1b2b3",
+    card:"#171721",
+    card_light: '#191924',
+    button:"#854CE6",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg:"#FFEADE",
+    bgLight: "#f0f0f0",
+    primary:"black",
+    text_primary:"#5F3008",
+    text_secondary:"#FF44AA",
+    card:"#FBD0D4",
+    button:"#5c5b5b",
+}
