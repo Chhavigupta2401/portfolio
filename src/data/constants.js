75207290c5452184903d8cc09d4bf2b19f6274f7
Chhavigupta2401 <chhavigupta2401@gmail.com>
2024-05-17 11:17:19 +0530

    reume

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Chhavigupta2401",
   resume:
-   "https://drive.google.com/file/d/1rS2_QVx3vCw5F68F36kEYgLKn5YzowSb/view?usp=drive_link",
+   "https://drive.google.com/file/d/1n7aJnBYaC-Em6BdWmhFxnZcumXVNgi2b/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/itsme-chhavi-gupta/",
 
 };
