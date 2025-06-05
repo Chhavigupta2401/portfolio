90acc85a901e2355739bdd90cadc26cd60650d4e
Chhavigupta2401 <chhavigupta2401@gmail.com>
2025-06-05 23:37:52 +0530

    message

---
+++ src/constants/index.js
@@ -0,0 +1,250 @@
+export const navLinks = [
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Experience',
+    href: '#experience',
+  },
+  {
+    id: 5,
+    name: 'Contact',
+    href: '#contact',
+  },
+];
+
+export const clientReviews = [
+  {
+    id: 1,
+    name: 'Khusboo Sharma',
+    position: 'Software Developer at Insights',
+    img: 'assets/review1.png',
+    review:
+      'Working with Chhavi was a fantastic experience. She transformed our outdated website into a modern, user-friendly platform. Her attention to detail and commitment to quality are unmatched. Highly recommend her for any web dev projects.',
+  },
+  {
+    id: 2,
+    name: 'Jaskrit Singh',
+    position: 'Founder of Sutra Wears',
+    img: 'assets/review2.png',
+    review:
+      'Chhavi’s expertise in designing is truly impressive. She delivered creative and innovative solution for our e-commerce site, and our online sales have significantly increased since the launch. She’s a true professional! Fantastic work.',
+  },
+  {
+    id: 3,
+    name: 'Rohan Malik',
+    position: 'Founder of Cybrus ',
+    img: 'assets/review3.png',
+    review:
+      'I can’t say enough good things about Chhavi. She was able to take our complex project requirements and turn them into a seamless, functional website. Her problem-solving abilities are outstanding.',
+  },
+  {
+    id: 4,
+    name: 'Roy',
+    position: 'CEO of BrightStar Enterprises',
+    img: 'assets/review4.png',
+    review:
+      'Chhavi was a pleasure to work with. She understood our requirements perfectly and delivered them that exceeded our expectations. Her skills in both dev and design are top-notch.',
+  },
+];
+
+export const myProjects = [
+  {
+    title: 'SkillAura - College guide app',
+    desc: 'SkillAura is a visionary college app helps to revolutionize the way students interact with their academic and personal growth journey. Combining visually appealing designs with a user-centric approach, SkillAura is crafted to deliver an intuitive and engaging experience.',
+    subdesc:
+      'Design as a unique service app with Figma, it is designed for optimal performance and scalability.',
+    href: 'https://www.behance.net/gallery/218935053/Student-Success-Hub',
+    texture: '/textures/project/skillaura.mp4',
+    logo: '',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+            {
+        id: 1,
+        name: 'Figma',
+        path: '/assets/figma.png',
+      },
+    ],
+  },
+  {
+    title: 'TwoTrees - Oil booking Interface',
+    desc: 'TwoTree Oil is a sleek and intuitive oil-booking app interface crafted with Figma, designed to streamline the process of purchasing oils effortlessly. The app offers a user-centric design, focusing on simplicity and functionality for an enhanced shopping experience.',
+    subdesc:
+      'this template serves as a playground for designers to push the boundaries of their design skills, improving their ability to create interactive and realistic app prototypes.',
+    href: 'https://www.figma.com/proto/dg6d4B883gaSgIKrawysFG/Two_tree?node-id=95-48&t=uBqHLaPev9Wb4I5g-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=95%3A18&show-proto-sidebar=1',
+    texture: '/textures/project/Two-Tree.mp4',
+    logo: '/assets/tt.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+       id: 1,
+        name: 'Figma',
+        path: '/assets/figma.png',
+      },
+
+    ],
+  },
+  {
+    title: 'Reimagine Apple Web',
+    desc: "A clone of Apple's iPhone 15 Pro website built using React.js and TailwindCSS. This project showcases effective use of GSAP (Greensock Animations) and Three.js for displaying iPhone 15 Pro models in various colors and shapes.",
+    subdesc:
+      'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
+    href: 'https://apple-project-zeta.vercel.app/',
+    texture: '/textures/project/iphone.mp4',
+    logo: 'https://apple-project-zeta.vercel.app/assets/images/apple.svg',
+    logoStyle: {
+        backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D'
+
+
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'JavaScript',
+        path: '/assets/js.png',
+      },
+      {
+        id: 4,
+        name: 'Html',
+        path: '/assets/html.png',
+      },
+    ],
+  },
+  {
+    title: 'Cybrus - Cyber Security Solution',
+    desc: 'As a Frontend Developer at Cybrus Inc., I played a key role in enhancing user experience by designing and implementing responsive web interfaces. Leveraging my expertise in HTML, CSS, and JavaScript, I collaborated with cross-functional teams to deliver high-quality, visually appealing, and user-friendly web applications.',
+    subdesc:'My work at Cybrus Inc. involved optimizing web performance, ensuring accessibility standards, and continuously improving the user journey through innovative design solutions and best coding practices',
+      href: 'https://cybrus-official.netlify.app/',
+    texture: '/textures/project/cybrus.mp4',
+    logo: '/assets/cybrus.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'CSS',
+        path: 'assets/css.png',
+      },
+      {
+        id: 3,
+        name: 'Javascript',
+        path: '/assets/js.png',
+      },
+      {
+        id: 4,
+        name: 'Html',
+        path: '/assets/html.png',
+      },
+    ],
+  },
+  {
+    title: 'Reimagine - marvel',
+    desc: ' Reimagine Marvel is a creative project aimed at giving the Marvel Cast Series a fresh and innovative look. Using tools like Figma and Canva, the project focuses on:',
+    subdesc:
+      'Prototyping: Crafting dynamic, interactive layouts to enhance viewer engagement.Wireframing: Structuring sleek designs to reimagine character presentation and storytelling.',
+    href: 'https://www.behance.net/gallery/207440117/Reimagine-Marvel',
+    texture: '/textures/project/marvel.mp4',
+    logo: '/assets/marvel.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'Figma',
+        path: '/assets/figma.png',
+      },
+
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  return {
+    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+  };
+};
+
+export const workExperiences = [
+  {
+    id: 1,
+    name: 'Cybrus',
+    pos: 'frontend Developer',
+    duration: 'Jan 2024 - Apr 2024',
+    title: "Working as a frontend developer, Building dyanamic Website and Providing Services for Testing.",
+    icon: '/assets/cybrus.png',
+    animation: 'victory',
+  },
+  {
+    id: 2,
+    name: 'Vipsgrow',
+    pos: 'Tech Department Intern',
+    duration: 'Mar 2024 - Jun 2024',
+    title: "Working as a developer and designer, Crafting user-centric and visually stunning websites that captivate and engage users.",
+    icon: '/assets/vip.png',
+    animation: 'clapping',
+  },
+  {
+    id: 3,
+    name: 'Internshala',
+    pos: 'Digital Marketing',
+    duration: 'DEc 2023 - Jan 2023',
+    title: "Collab with teams to working on marketing and digital services for clients.",
+    icon: '/assets/intern.png',
+    animation: 'salute',
+  },
+];
